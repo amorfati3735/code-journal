153e7a2c5ca4996835f456be9d7de0bd76295c12
pratik <pratikambastha5@gmail.com>
2026-02-05 23:23:18 +0530

    [05-Feb][23.23]

--- linked lists/basics.c
+++ linked lists/basics.c
@@ -6,6 +6,18 @@ struct node
     int data;
     struct node *next;
 };
+void printer(struct node *head)
+{
+    struct node *temp = head;
+    while (temp != NULL)
+    {
+        printf("newer: %d \n", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+    return;
+}
+
 int main()
 {
     // first construct a normal ll
@@ -16,24 +28,27 @@ int main()
     l1->data = 20;
     l1->next = l2;
 
-    l2->data = 30;
+    l2->data = 500;
     l2->next = NULL;
 
     head = l1;
-    return 0;
 
     // now traverse and print everything
     struct node *temp = head;
-    while (temp != NULL)
-    {
-        printf("%d", temp->data);
-        temp = temp->next; // this is moving forward
-    }
+    printer(head);
+
     // now insert a new node bw l1, l2 that has data=30
     struct node *new = malloc(sizeof(struct node));
     new->data = 30;
     l1->next = new;
-    new->next = l2; // this is supposedly unsafe, whatever fuck it
+    new->next = l2;
 
+    // printer(struct node * head); you did this. this is declaration, remove the type
+    printer(head);
+    // deletion of a node
+
+    l1->next = l2;
+    printer(head);
+    free(new);
     return 0;
 }
